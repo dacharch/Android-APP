a4274f6aee536e0d6b514b2dc691abf286f27111
Neeraj Kumar <nk35715945@gmail.com>
2024-10-13 11:27:59 +0530

    Calculator component is done

--- components/Calculator/Calculator.tsx
+++ components/Calculator/Calculator.tsx
@@ -7,36 +7,108 @@ const Calculator = () => {
   const[value,setValue] = useState('0') ;
   const [bracketopen,setBracketOpen] =useState(false) ;
 
+  const handlePress = (val:any) => {
+    if (val == 'AC') {
+        setValue('0')
+    }
+    else if (val == '=') {
+        try {
+            if ((value.match(/\(/g) || []).length == (value.match(/\)/g) || []).length) {
 
+                if (value.slice(-1) == '+' || value.slice(-1) == '-' || value.slice(-1) == '*' || value.slice(-1) == '/') {
+                    setValue(`${eval(value.replace('()', '(0)').slice(0, -1))}`)
+                }
+                else {
+                    setValue(`${eval(value.replace('()', '(0)') + '*1')}`)
+                }
+            }
+        }
+        catch (e) {
+            setValue('Format Error')
+        }
+    }
+    else if (val == 'back') {
+        setValue(value.slice(0, -1))
+    }
+    else if (val == '()') {
+        if (value == '0') {
+            setValue('(')
+            setBracketOpen(true)
+
+        }
+        else if (value.slice(-1) == '+' || value.slice(-1) == '-' || value.slice(-1) == '*' || value.slice(-1) == '/') {
+            setValue(value + '(')
+            setBracketOpen(true)
+        }
+        else {
+            if (bracketopen == true) {
+                setValue(value + ')')
+                setBracketOpen(false)
+            }
+            else {
+                setValue(value + '(')
+                setBracketOpen(true)
+            }
+        }
+    }
+    else {
+        if (value == '0') {
+            if (val == '+' || val == '-' || val == '*' || val == '/' || val == '.' || val == '%') {
+                setValue(value + val)
+            }
+            else {
+                setValue(val)
+            }
+        }
+        // console.log(val)
+        else if (isNaN(val)) {
+            console.log(value.slice(-1))
+            if (value.slice(-1) == '+' || value.slice(-1) == '-' || value.slice(-1) == '*' || value.slice(-1) == '/' || value.slice(-1) == '.' || value.slice(-1) == '%') {
+                setValue(value.slice(0, -1) + val)
+            }
+            else {
+                setValue(value + val)
+            }
+        }
+        else if (!isNaN(val)) {
+            setValue(value + val)
+        }
+    }
+
+
+} 
+
+  
   return (
     <View style={styles.main_screen}>
-    <ScrollView style={styles.main_screen__display}  >
+    <ScrollView style={styles.main_screen__display} ref={ref => { this.scrollView = ref }}
+        onContentSizeChange={() => this.scrollView.scrollToEnd({ animated: true })}>
         <Text style={styles.main_screen__display_text}>
             {value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
         </Text>
     </ScrollView>
     <View style={styles.main_screen__keypad}>
         <View style={styles.main_screen__keypad_row}>
-            <Pressable >
+            <Pressable onPress={() => handlePress('AC')}>
                 <View style={styles.btn1_outer}>
                     <Text style={styles.bg1_button} >AC</Text>
                 </View>
             </Pressable>
 
-            <Pressable >
+            <Pressable onPress={() => handlePress('()')}>
                 <View style={styles.btn2_outer}>
                     <Text style={styles.bg2_button} >( )</Text>
                 </View>
             </Pressable>
 
-            <Pressable >
+            <Pressable onPress={() => handlePress('%')}>
                 <View style={styles.btn2_outer}>
                     <Text style={styles.bg2_button} >%</Text>
                 </View>
             </Pressable>
 
 
-            <Pressable >
+            <Pressable onPress={() => handlePress('/')}>
                 <View style={styles.btn2_outer}>
                     <Text style={styles.bg2_button} >/</Text>
                 </View>
@@ -45,24 +117,24 @@ const Calculator = () => {
         </View>
         {/*  */}
         <View style={styles.main_screen__keypad_row}>
-            <Pressable>
+            <Pressable onPress={() => handlePress('7')}>
                 <View style={styles.btn_outer}>
                     <Text style={styles.bg_button} >7</Text>
                 </View>
             </Pressable>
-            <Pressable >
+            <Pressable onPress={() => handlePress('8')}>
                 <View style={styles.btn_outer}>
                     <Text style={styles.bg_button} >8</Text>
                 </View>
             </Pressable>
 
-            <Pressable >
+            <Pressable onPress={() => handlePress('9')}>
                 <View style={styles.btn_outer}>
                     <Text style={styles.bg_button} >9</Text>
                 </View>
             </Pressable>
 
-            <Pressable >
+            <Pressable onPress={() => handlePress('*')}>
                 <View style={styles.btn2_outer}>
                     <Text style={styles.bg2_button} >*</Text>
                 </View>
@@ -70,25 +142,25 @@ const Calculator = () => {
         </View>
         {/*  */}
         <View style={styles.main_screen__keypad_row}>
-            <Pressable >
+            <Pressable onPress={() => handlePress('4')}>
                 <View style={styles.btn_outer}>
                     <Text style={styles.bg_button} >4</Text>
                 </View>
             </Pressable>
 
-            <Pressable>
+            <Pressable onPress={() => handlePress('5')}>
                 <View style={styles.btn_outer}>
                     <Text style={styles.bg_button} >5</Text>
                 </View>
             </Pressable>
 
-            <Pressable >
+            <Pressable onPress={() => handlePress('6')}>
                 <View style={styles.btn_outer}>
                     <Text style={styles.bg_button} >6</Text>
                 </View>
             </Pressable>
 
-            <Pressable >
+            <Pressable onPress={() => handlePress('-')}>
                 <View style={styles.btn2_outer}>
                     <Text style={styles.bg2_button} >-</Text>
                 </View>
@@ -96,25 +168,25 @@ const Calculator = () => {
         </View>
         {/*  */}
         <View style={styles.main_screen__keypad_row}>
-            <Pressable >
+            <Pressable onPress={() => handlePress('1')}>
                 <View style={styles.btn_outer}>
                     <Text style={styles.bg_button} >1</Text>
                 </View>
             </Pressable>
 
-            <Pressable >
+            <Pressable onPress={() => handlePress('2')}>
                 <View style={styles.btn_outer}>
                     <Text style={styles.bg_button} >2</Text>
                 </View>
             </Pressable>
 
-            <Pressable >
+            <Pressable onPress={() => handlePress('3')}>
                 <View style={styles.btn_outer}>
                     <Text style={styles.bg_button} >3</Text>
                 </View>
             </Pressable>
 
-            <Pressable >
+            <Pressable onPress={() => handlePress('+')}>
                 <View style={styles.btn2_outer}>
                     <Text style={styles.bg2_button} >+</Text>
                 </View>
@@ -122,25 +194,25 @@ const Calculator = () => {
         </View>
         {/*  */}
         <View style={styles.main_screen__keypad_row}>
-            <Pressable >
+            <Pressable onPress={() => handlePress('0')}>
                 <View style={styles.btn_outer}>
                     <Text style={styles.bg_button} >0</Text>
                 </View>
             </Pressable>
 
-            <Pressable>
+            <Pressable onPress={() => handlePress('.')}>
                 <View style={styles.btn2_outer}>
                     <Text style={styles.bg2_button} >.</Text>
                 </View>
             </Pressable>
 
-            <Pressable >
+            <Pressable onPress={() => handlePress('back')}>
                 <View style={styles.btn1_outer}>
                     <Text style={styles.bg1_button} >&lt;</Text>
                 </View>
             </Pressable>
 
-            <Pressable >
+            <Pressable onPress={() => handlePress('=')}>
                 <View style={styles.btn2_outer}>
                     <Text style={styles.bg2_button} >=</Text>
                 </View>
@@ -180,15 +252,12 @@ main_screen__display_text: {
     fontSize: 50,
     textAlign: 'right',
 },
-// ////////////////////////////////////////////////////////////////
 
 
 main_screen__keypad: {
     width: '100%',
     height: '70%',
-    // backgroundColor: '#FF5757',
     display: 'flex',
-    // justifyContent: 'center',
 },
 main_screen__keypad_row: {
     display: 'flex',
